3090a49d17bff3fb2e958fe89cec1fc0a18f9d58
yumetodo <yume-wikijp@live.jp>
2016-02-28 13:47:35 +0900

    remove TIME_ELAPSED

--- KCS_CUI/source/base.hpp
+++ KCS_CUI/source/base.hpp
@@ -28,16 +28,6 @@ using std::tuple;
 using std::unordered_map;
 using std::vector;
 
-#define TIME_ELAPSED(...) time_elapsed_lambda([&]{__VA_ARGS__;})
-template <typename inner_loop>
-void time_elapsed_lambda(inner_loop body)
-{
-	auto t0 = std::chrono::high_resolution_clock::now();
-	body();
-	auto t1 = std::chrono::high_resolution_clock::now();
-	cout << "処理時間：" << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count() << "[ms]\n";
-}
-
 const int kBattleSize = 2;		//戦闘で戦うのは敵と味方の「2つ」
 const int kMaxFleetSize = 2;	//
 const int kMaxUnitSize = 6;		//艦隊に含まれる最大艦数
--- KCS_CUI/source/main.cpp
+++ KCS_CUI/source/main.cpp
@@ -6,38 +6,43 @@
 #include "fleet.hpp"
 #include "result.hpp"
 #include "simulator.hpp"
-
+#define KCS_MEASURE_PROCESS_TIME
 int main(int argc, char *argv[]) {
 	try {
-//		TIME_ELAPSED(
-			// 現在の設定を取得する
-			Config config(argc, argv);
-			config.Put();
-			// データベースを読み込む
-			WeaponDB weapon_db;
-			weapon_db.Get(124).Put();
-			KammusuDB kammusu_db;
-			kammusu_db.Get(450, 155).Reset(weapon_db).Put();
-			// ファイルから艦隊を読み込む
-			vector<Fleet> fleet(kBattleSize);
-			for (auto i = 0; i < kBattleSize; ++i) {
-				fleet[i] = Fleet(config.GetInputFilename(i), config.GetFormation(i), weapon_db, kammusu_db);
-				fleet[i].Put();
-			}
-			// シミュレータを構築し、並列演算を行う
-			std::random_device rd;
-			auto seed = rd();
-			vector<Result> result_db(config.GetTimes());
-			//#pragma omp parallel for num_threads(config.Threads())
-			for (int n = 0; n < config.GetTimes(); ++n) {
-				Simulator simulator(fleet, seed + n);
-				result_db[n] = simulator.Calc();
-			}
-			// 集計を行う
-			for (int n = 0; n < config.GetTimes(); ++n) {
-				cout << result_db[n].Put() << "\n";
-			}
-//		);
+#if defined(KCS_MEASURE_PROCESS_TIME)
+		const auto process_begin_time = std::chrono::high_resolution_clock::now();
+#endif
+		// 現在の設定を取得する
+		Config config(argc, argv);
+		config.Put();
+		// データベースを読み込む
+		WeaponDB weapon_db;
+		weapon_db.Get(124).Put();
+		KammusuDB kammusu_db;
+		kammusu_db.Get(450, 155).Reset(weapon_db).Put();
+		// ファイルから艦隊を読み込む
+		vector<Fleet> fleet(kBattleSize);
+		for (auto i = 0; i < kBattleSize; ++i) {
+			fleet[i] = Fleet(config.GetInputFilename(i), config.GetFormation(i), weapon_db, kammusu_db);
+			fleet[i].Put();
+		}
+		// シミュレータを構築し、並列演算を行う
+		std::random_device rd;
+		auto seed = rd();
+		vector<Result> result_db(config.GetTimes());
+		//#pragma omp parallel for num_threads(config.Threads())
+		for (int n = 0; n < config.GetTimes(); ++n) {
+			Simulator simulator(fleet, seed + n);
+			result_db[n] = simulator.Calc();
+		}
+		// 集計を行う
+		for (int n = 0; n < config.GetTimes(); ++n) {
+			cout << result_db[n].Put() << "\n";
+		}
+#if defined(KCS_MEASURE_PROCESS_TIME)
+		const auto process_end_time = std::chrono::high_resolution_clock::now();
+		cout << "処理時間：" << std::chrono::duration_cast<std::chrono::milliseconds>(process_end_time - process_begin_time).count() << "[ms]" << endl;
+#endif
 	}
 	catch (const std::exception& er) {
 		std::cerr << er.what() << std::endl;
