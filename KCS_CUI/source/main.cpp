d4ce54887e20b8d027307d461af48d05c056691d
yumetodo <yume-wikijp@live.jp>
2016-03-04 03:51:39 +0900

    #22 omp_get_thread_num()を使うように

--- KCS_CUI/source/main.cpp
+++ KCS_CUI/source/main.cpp
@@ -7,6 +7,7 @@
 #include "result.hpp"
 #include "simulator.hpp"
 #include "random.hpp"
+#include <omp.h>//omp_get_thread_num()
 #define KCS_MEASURE_PROCESS_TIME
 int main(int argc, char *argv[]) {
 	try {
@@ -31,7 +32,7 @@ int main(int argc, char *argv[]) {
 		#pragma omp parallel for num_threads(config.GetThreads())
 		for (int n = 0; n < config.GetTimes(); ++n) {
 			vector<Fleet> fleet_ = fleet;	//ハードコピーしないと徐々に体力が削られるだけなのでダメ
-			Simulator simulator(fleet_, seed[n], kSimulateModeDN);	//戦闘のたびにSimulatorインスタンスを設定する
+			Simulator simulator(fleet_, seed[omp_get_thread_num()], kSimulateModeDN);	//戦闘のたびにSimulatorインスタンスを設定する
 			result_db[n] = simulator.Calc();
 		}
 #if defined(KCS_MEASURE_PROCESS_TIME)
