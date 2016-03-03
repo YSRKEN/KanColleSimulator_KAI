f7d066319bced31b01a433728de43cc279df9f5f
yumetodo <yume-wikijp@live.jp>
2016-03-03 12:51:32 +0900

    use pimpl for config class

--- KCS_CUI/source/config.hpp
+++ KCS_CUI/source/config.hpp
@@ -1,27 +1,35 @@
 ﻿#pragma once
 #include <iostream>
+#include <memory>
 // 設定クラス
 class Config {
-	vector<string> input_filename_;	//入力ファイル名
-	vector<Formation> formation_;		//陣形指定
-	int times_;		//試行回数
-	int threads_;	//スレッド数
-	string output_filename_;	//出力ファイル名
-	bool json_prettify_flg_;	//出力ファイルを整形するか
+	struct Impl;
+	std::unique_ptr<Impl> pimpl;
+	//vector<string> input_filename_;	//入力ファイル名
+	//vector<Formation> formation_;		//陣形指定
+	//int times_;		//試行回数
+	//int threads_;	//スレッド数
+	//string output_filename_;	//出力ファイル名
+	//bool json_prettify_flg_;	//出力ファイルを整形するか
 public:
 	// コンストラクタ
-	Config() {}
+	Config();
 	Config(int argc, char *argv[]);
+	~Config();
+	Config(const Config&) = delete;
+	Config(Config&&);
+	Config& operator=(const Config&) = delete;
+	Config& operator=(Config&&);
 	// 中身を表示する
 	void Put() const;
 	// getter
-	const string& GetInputFilename(const int n) const noexcept { return input_filename_[n]; }
+	const string& GetInputFilename(const int n) const noexcept;
 	std::wstring GetInputFilenameW(const int n) const;
-	Formation GetFormation(const int n) const noexcept { return formation_[n]; }
-	int GetTimes() const noexcept { return times_; }
-	int GetThreads() const noexcept { return threads_; }
-	const string& GetOutputFilename() noexcept { return output_filename_; }
-	bool GetJsonPrettifyFlg() const noexcept { return json_prettify_flg_; }
+	Formation GetFormation(const int n) const noexcept;
+	int GetTimes() const noexcept;
+	int GetThreads() const noexcept;
+	const string& GetOutputFilename() noexcept;
+	bool GetJsonPrettifyFlg() const noexcept;
 	//
 	friend std::ostream& operator<<(std::ostream& os, const Config& conf);
 	friend std::wostream& operator<<(std::wostream& os, const Config& conf);
