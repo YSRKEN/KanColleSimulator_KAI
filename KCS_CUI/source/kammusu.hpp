f758555ef8e70325940696a288efc1bd109fb357
YSRKEN <ysr.ken@gmail.com>
2016-03-07 22:35:32 +0900

    3-5艦隊を追加、#pragma onceを#defineガードに変更

--- KCS_CUI/source/kammusu.hpp
+++ KCS_CUI/source/kammusu.hpp
@@ -1,4 +1,5 @@
-﻿#pragma once
+﻿#ifndef KCS_KAI_KAMMUSU_H_
+#define KCS_KAI_KAMMUSU_H_
 
 #include "weapon.hpp"
 #include <iostream>
@@ -209,3 +210,5 @@ namespace detail {
 	}
 }
 constexpr detail::ToSpeed_helper ToSpeed() { return{}; }
+
+#endif KCS_KAI_KAMMUSU_H_
