39d38b3ddd375a3234ece4ec40c459291f4f9759
YSRKEN <ysr.ken@gmail.com>
2016-03-28 07:12:36 +0900

    使っていなかったJSONとBATを削除し、version.hを修正した

--- run.bat
+++
@@ -1,2 +0,0 @@
-KCS_CUI -i "sample\fleet sample1.json" "sample\fleet sample2.json" -f 0 1 -n 1
-pause
--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 #ifndef KCS_VERSION_H_
 #define KCS_VERSION_H_
-#define KCS_VERSION  1,5,0
+#define KCS_VERSION  2,0,0
 #define KCS_VERSION_STR "2.0.0"
 #define KCS_COPYRIGHT_STR "Copyright (c) 2016 KanColleSimulator project"
 #define KCS_FILE_DESCRIPTION "Simulate KanColle battle."
