881a719d06e11f401c5286d56cb2c26ed7c38c4d
yumetodo <yume-wikijp@live.jp>
2016-10-24 12:03:17 +0900

    change check_csv_format to return fail count

--- spec/csv_spec.rb
+++ spec/csv_spec.rb
@@ -4,33 +4,32 @@ def check_csv_format(loaded_csv, regex)
   loaded_csv.each_with_index{|line, i|
     next if i == 0
     unless line =~ regex
+      if 0 == fail_count
+        puts 'fail line :'
+      end
       puts line
-      puts 'false'
       fail_count += 1
     end
   }
-  if 0 != fail_count
-    puts fail_count
-  end
-  return 0 == fail_count
+  return fail_count
 end
 describe "CSV test" do
   it "ships.csv" do
     expect(check_csv_format(
       File.read('ships.csv', encoding:'Shift_JIS').split("\n"), 
       /[0-9]+,[^,]+,[0-9]+,[0-9]+\/[0-9]+.[0-9]+\/[0-9]+,[0-9]+\/[0-9]+,[0-9]+\/[0-9]+,[0-9]+\/[0-9]+,[0-9]+\/[0-9]+,[0-9]+,[0-9],[0-9],[0-9]+\/[0-9]+\/[0-9]+\/[0-9]+\/[0-9]+,[0-9]+\/[0-9]+,[0-9]+\/[0-9]+,[0-9]+\/[0-9]+,[-0-9]+\/[-0-9]+\/[-0-9]+\/[-0-9]+\/[-0-9]+,[0-9]/
-    )).to eq true
+    )).to eq 0
   end
   it "ships_all.csv" do
     expect(check_csv_format(
       File.read('ships_all.csv', encoding:'Shift_JIS').split("\n"), 
       /[0-9]+,[^,]+,[0-9]+,([0-9]+\/[0-9]+|null),([0-9]+\/[0-9]+|null),([0-9]+\/[0-9]+|null),([0-9]+\/[0-9]+|null),([0-9]+\/[0-9]+|null),([0-9]+\/[0-9]+|null),([0-9]+|null),([0-9]+|null),([0-9]+|null),([0-9]+\/[0-9]+\/[0-9]+\/[0-9]+\/[0-9]+|null),([0-9]+\/[0-9]+|null),([0-9]+\/[0-9]+|null),([0-9]+\/[0-9]+|null),([-0-9]+\/[-0-9]+\/[-0-9]+\/[-0-9]+\/[-0-9]+|null),[0-9]/
-    )).to eq true
+    )).to eq 0
   end
   it "slotitems.csv" do
     expect(check_csv_format(
       File.read('slotitems.csv', encoding:'Shift_JIS').split("\n"), 
       /[0-9]+,[^,]+,[^,]+,[-0-9]+,[-0-9]+,[-0-9]+,[-0-9]+,[-0-9]+,[-0-9]+,[-0-9]+,[-0-9]+,[-0-9]+,[-0-9]+/
-    )).to eq true
+    )).to eq 0
   end
 end
