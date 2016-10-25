3b097a0eecba7eb04b758f564b5dc6dd11cd833f
yumetodo <yume-wikijp@live.jp>
2016-10-25 23:24:31 +0900

    96式150cm探照灯を追加

--- KCS_CUI/source/weapon.hpp
+++ KCS_CUI/source/weapon.hpp
@@ -50,11 +50,13 @@ enum class WeaponClass : std::uint64_t {
 	CR         = 0x0000000020000000,	//戦闘糧食
 	OS         = 0x0000000040000000,	//洋上補給
 	WF         = 0x0000000080000000,	//水上戦闘機
+	SL_94_150  = 0x0000000100000000,    //96式150cm探照灯
 	Other      = 0x8000000000000000,	//その他
 	AirFight = PF | PBF | PB | WB | PA | WF,					//航空戦に参加する艦載機
 	AirTrailer = PA | PS | PSS | DaiteiChan | WS | WSN | WF,	//触接に参加する艦載機
 	AirBomb = PBF | PB | WB | PA,								//開幕爆撃に参加する艦載機
 	Air = (AirFight | AirTrailer) ^ (WS | WSN),					//(熟練度が存在する)艦載機
+	IsSLKindMask = SL | SL_94_150,
 };
 constexpr inline auto operator|(const WeaponClass& l, const WeaponClass& r) { return static_cast<WeaponClass>(static_cast<std::underlying_type_t<WeaponClass>>(l) | static_cast<std::underlying_type_t<WeaponClass>>(r)); }
 template<class E, class T>
@@ -100,6 +102,7 @@ namespace detail {
 		{ L"戦闘糧食", WeaponClass::CR },
 		{ L"洋上補給", WeaponClass::OS },
 		{ L"水上戦闘機", WeaponClass::WF },
+		{ L"96式150cm探照灯", WeaponClass::SL_94_150 },
 		{ L"その他", WeaponClass::Other },
 	};
 }
