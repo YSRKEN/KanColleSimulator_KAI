9933a9fad9ce7ae0b358d048162446fbec6b4b9c
yumetodo <yume-wikijp@live.jp>
2017-01-13 01:08:02 +0900

    try to append double-quote(fail)
    スペースが有るとうまく動いていない

--- csv_convert.bat
+++ csv_convert.bat
@@ -18,6 +18,10 @@ for /f "delims=" %%s in (%1) do (
 )
 exit /b 0
 
+rem @brief convert line convert
+rem @param source_line_text source line text
+rem @param prefix C-Preprocesser-Macro-Function name
+rem @param out output file full path
 :line_convert
 setlocal enabledelayedexpansion
 
@@ -30,13 +34,37 @@ set out=%~3
 rem replace `/` to `.`
 set source_line_text=%source_line_text:/=.%
 
+rem get first element
+for /f "delims=, tokens=1" %%s in ("%source_line_text%") do set re=%%s
+rem get second first element and concat
+for /f "delims=, tokens=2" %%s in ("%source_line_text%") do set re=%re%,"%%s",
+
+call :strlen "third_elem_pos" "%re%"
+set /a third_elem_pos-=2
+
 rem `)` should be escaped by `^`
 set postfix=,^)
 rem concat strings
-set re=%prefix%%source_line_text%%postfix%
+set re=%prefix%%re%!source_line_text:~%third_elem_pos%!%postfix%
 echo %re%>>%out%
 endlocal
 exit /b 0
+
+:strlen
+setlocal enabledelayedexpansion
+set result_env_name=%~1
+set str=%~2
+
+set /a count=0
+
+:strlen_loop
+if not defined str goto strlen_loop_break
+set str=%str:~1%
+set /a count+=1
+
+goto strlen_loop
+:strlen_loop_break
+endlocal && set /a %result_env_name%=%count% && exit /b 0
 
 :error
 echo "invalid argument detect." 1> &2
